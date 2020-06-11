6975c8b7d36cc7e7b748b2e4c61fc9a0eb9e9e5c
ferdy <xv.ferdy@gmail.com>
2020-06-11 13:56:40 +0700

    test html

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('hi ferdy');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send(`
+    <div>
+    <form>
+      <input placeholder="email" />
+      <input placeholder="password" />
+      <input placeholder="password confirmation" />
+      <button>Sign Up</button>
+    </form>
+  </div>
+    `);
+});
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+});
