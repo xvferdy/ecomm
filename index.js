5de920be61ddf7a751821ee57c58fb6964cc6d1b
ferdy <xv.ferdy@gmail.com>
2020-06-15 19:00:17 +0700

    Route handler with body-parser

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 const express = require('express');
+const bodyParser = require("body-parser");
+
 const app = express();
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.get('/', (req, res) => {
-    res.send(`
+  res.send(`
     <div>
-    <form>
-      <input placeholder="email" />
-      <input placeholder="password" />
-      <input placeholder="password confirmation" />
+    <form method="POST">
+      <input name="email" placeholder="email" />
+      <input name="password" placeholder="password" />
+      <input name="passwordConfirmation" placeholder="password confirmation" />
       <button>Sign Up</button>
     </form>
   </div>
     `);
 });
 
+app.post('/', (req, res) => {
+  console.log(req.body.email);
+  res.send('Account Created!');
+});
+
 app.listen(3000, () => {
-    console.log('Listening on port 3000');
+  console.log('Listening on port 3000');
 });
