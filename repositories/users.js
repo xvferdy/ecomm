16fb1e9bf43842d85d4ba1aa76e26354a20676d2
ferdy <xv.ferdy@gmail.com>
2020-06-16 20:26:31 +0700

    Refactor read file (getAll())

--- repositories/users.js
+++ repositories/users.js
@@ -12,6 +12,21 @@ class UsersRepository {
             fs.writeFileSync(this.filename, '[]')
         }
     }
+
+    async getAll() {
+        return JSON.parse(await fs.promises.readFile(this.filename, {
+            encoding: 'utf8'
+        }));
+
+    }
 }
 
-new UsersRepository('users.json');
+const test = async () => {
+    const repo = new UsersRepository('users.json');
+
+    const users = await repo.getAll();
+
+    console.log(users);
+}
+
+test();
