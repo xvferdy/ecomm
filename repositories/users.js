6ac9c8a41d5d20426066747da43dada2b36c0b29
ferdy <xv.ferdy@gmail.com>
2020-06-18 21:00:44 +0700

    Implement create() method

--- repositories/users.js
+++ repositories/users.js
@@ -7,9 +7,9 @@ class UsersRepository {
         }
         this.filename = filename;
         try {
-            fs.accesSync(this.filename)
+            fs.accessSync(this.filename);
         } catch (err) {
-            fs.writeFileSync(this.filename, '[]')
+            fs.writeFileSync(this.filename, '[]');
         }
     }
 
@@ -17,12 +17,25 @@ class UsersRepository {
         return JSON.parse(await fs.promises.readFile(this.filename, {
             encoding: 'utf8'
         }));
+    }
 
+    async create(attrs) {
+        // { email: "asdasd@gmail.com", pass:"asdasda" }
+        const records = await this.getAll();
+        records.push(attrs);
+
+        // write the update 'records' array back to this.filename (user.json)
+        await fs.promises.writeFile(this.filename, JSON.stringify(records))
     }
 }
 
 const test = async () => {
     const repo = new UsersRepository('users.json');
+
+    await repo.create({
+        email: "asdad@SpeechGrammarList.com",
+        pass: "asdasd"
+    })
 
     const users = await repo.getAll();
 
