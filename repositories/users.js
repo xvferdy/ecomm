9717b7057e724a996d4a3bd7da353be1023ce6d4
ferdy <xv.ferdy@gmail.com>
2020-06-23 02:25:06 +0700

    Implement delete and update method

--- repositories/users.js
+++ repositories/users.js
@@ -32,6 +32,7 @@ class UsersRepository {
         await this.writeAll(records);
     }
 
+    // mengedit data asli pada harddrive (diambil dari memory array getAll())
     async writeAll(records) {
         await fs.promises.writeFile(this.filename, JSON.stringify(records, null, 2));
     }
@@ -44,23 +45,48 @@ class UsersRepository {
         const records = await this.getAll();
         return records.find(record => record.id === id); //iterate masing2 index
     }
+
+    async delete(id) {
+        const records = await this.getAll();
+        const filteredRecords = records.filter(record => record.id !== id);
+        await this.writeAll(filteredRecords);
+    }
+
+    async update(id, attrs) {
+        const records = await this.getAll();
+        const record = records.find(record => record.id === id);
+
+        if (!record) {
+            throw new Error(`Record with id ${id} not found`);
+        }
+
+        // record === { email: 'test@gmail.com }
+        // attrs === { pass: 'mypass' }
+        Object.assign(record, attrs);
+        // record === { email: 'test@gmail.com, pass: 'mypass' }
+        await this.writeAll(records);
+    }
 }
 
 const test = async () => {
     const repo = new UsersRepository('users.json');
 
     // await repo.create({
-    //     email: "asdad@SpeechGrammarList.com",
-    //     pass: "asdasd"
+    //     email: "ferdy@gmail.com",
+    //     pass: "ayam"
     // })
 
     // const users = await repo.getAll();
-
     // console.log(users);
 
-    const user = await repo.getOne('05a175d7');
+    // const user = await repo.getOne('05a175d7');
+    // console.log(user);
 
-    console.log(user);
+    // await repo.delete("09d96a1a");
+
+    await repo.update("59c99e98", {
+        type: "reguler"
+    });
 }
 
 test();
