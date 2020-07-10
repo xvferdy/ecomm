6f49c84491600f7198bc7821f3468d13adb64b74
ferdy <xv.ferdy@gmail.com>
2020-07-10 12:19:40 +0700

    download style template

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 const express = require('express');
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const authRouter = require('./routes/admin/auth');
 
 const app = express();
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+
+app.use(express.static('public'));
+app.use(
+	bodyParser.urlencoded({
+		extended: true
+	})
+);
 
 app.use(
-  cookieSession({
-    keys: ['jekyll123']
-  })
+	cookieSession({
+		keys: [ 'jekyll123' ]
+	})
 );
 
 app.use(authRouter);
 
 //terminal
 app.listen(3000, () => {
-  console.log('Listening on port 3000');
+	console.log('Listening on port 3000');
 });
