832114d2beb1f0436209df3c4baad890358199fe
ferdy <xv.ferdy@gmail.com>
2020-07-27 06:09:17 +0700

    Create cart session

--- routes/carts.js
+++ routes/carts.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const cartsRepo = require('../repositories/carts');
 
 const router = express.Router();
 // Receive a POST request to add an item to a cart
-router.post('/cart/products', (req, res) => {
-	console.log(req.body.productId);
+router.post('/cart/products', async (req, res) => {
+	// Figure out the cart
+	let cart;
+	if (!req.session.cartId) {
+		// We dont have cart, need to create one & make new session
+		cart = await cartsRepo.create({ item: [] });
+		req.session.cartId = cart.id;
+	} else {
+		// We have cart, lets get it from repository
+		cart = await cartsRepo.getOne(req.session.cartId);
+	}
+
+	console.log(cart);
+	// Either increment quantity for existing product
+	// Or add new product
 
 	res.send('Product added to cart!');
 });
