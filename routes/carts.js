bcefeb7e84aaf3a2dce26a77a03f28b0d7d6612b
ferdy <xv.ferdy@gmail.com>
2020-07-27 07:32:38 +0700

    Add item to cart repo

--- routes/carts.js
+++ routes/carts.js
@@ -4,6 +4,8 @@ const cartsRepo = require('../repositories/carts');
 const router = express.Router();
 // Receive a POST request to add an item to a cart
 router.post('/cart/products', async (req, res) => {
+	// console.log(req.body); //{ productId: '62b582ba' }
+
 	// Figure out the cart
 	let cart;
 	if (!req.session.cartId) {
@@ -15,10 +17,19 @@ router.post('/cart/products', async (req, res) => {
 		cart = await cartsRepo.getOne(req.session.cartId);
 	}
 
-	console.log(cart);
+	// console.log(cart); //{ items: [], id: 'aa3351b1' }
 
-	// Either increment quantity for existing product
-	// Or add new product
+	// Add item to cart
+	// Increment quantity for existing product
+	const existingItem = cart.items.find((item) => item.id === req.body.productId);
+	if (existingItem) {
+		existingItem.quantity++;
+	} else {
+		// Or add new product
+		cart.items.push({ id: req.body.productId, quantity: 1 });
+	}
+
+	await cartsRepo.update(req.session.cartId, { items: cart.items });
 
 	res.send('Product added to cart!');
 });
